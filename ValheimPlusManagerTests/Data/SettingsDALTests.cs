6aec41d773540869152cf865207995acf4e6d742
Robert Roos <robert@iodesk.net>
2021-03-03 05:09:51 +0100

    Unit testing project added

---
+++ ValheimPlusManagerTests/Data/SettingsDALTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ValheimPlusManager.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValheimPlusManager.Data.Tests
+{
+    [TestClass()]
+    public class SettingsDALTests
+    {
+        [TestMethod()]
+        [Timeout(2000)]
+        public void GetSettingsTest()
+        {
+            Assert.IsNotNull(SettingsDAL.GetSettings());
+        }
+    }
+}
