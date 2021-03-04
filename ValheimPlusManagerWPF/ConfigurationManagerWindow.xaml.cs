cdb95f4b5d62665a35b56031879a1f998c58b9be
Robert Roos <robert@iodesk.net>
2021-03-04 17:21:03 +0100

    Bugfix for negative values

--- ValheimPlusManagerWPF/ConfigurationManagerWindow.xaml.cs
+++ ValheimPlusManagerWPF/ConfigurationManagerWindow.xaml.cs
@@ -55,13 +55,13 @@ namespace ValheimPlusManager
 
         private void IntValidationTextBox(object sender, TextCompositionEventArgs e)
         {
-            Regex regex = new Regex("[^0-9]+");
+            Regex regex = new Regex("[^-0-9]+");
             e.Handled = regex.IsMatch(e.Text);
         }
 
         private void FloatValidationTextBox(object sender, TextCompositionEventArgs e)
         {
-            Regex regex = new Regex("[0-9]+(\\.[0-9]?)?");
+            Regex regex = new Regex("[-0-9]+(\\.[-0-9]?)?");
             e.Handled = regex.IsMatch(e.Text);
         }
     }
