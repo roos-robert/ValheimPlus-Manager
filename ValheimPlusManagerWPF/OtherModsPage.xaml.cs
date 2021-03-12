d2ddfd90f1c1691efe98e3c952225f8792e76627
Robert Roos <robert@iodesk.net>
2021-03-12 19:41:30 +0100

    Testing out addition of other mods

--- ValheimPlusManagerWPF/OtherModsPage.xaml.cs
+++ ValheimPlusManagerWPF/OtherModsPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ValheimPlusManager.Data;
+using ValheimPlusManager.Models;
+using ValheimPlusManager.SupportClasses;
 
 namespace ValheimPlusManager
 {
@@ -20,9 +25,52 @@ namespace ValheimPlusManager
     /// </summary>
     public partial class OtherModsPage : Page
     {
+        private bool ValheimPlusInstalledClient { get; set; } = false;
+        private bool ValheimPlusInstalledServer { get; set; } = false;
+        private Settings Settings { get; set; }
+
+        SnackbarMessageQueue myMessageQueue = new SnackbarMessageQueue(TimeSpan.FromMilliseconds(3000));
+
         public OtherModsPage()
         {
             InitializeComponent();
+            statusSnackBar.MessageQueue = myMessageQueue;
+            FetchSettings();
+        }
+
+        public void FetchSettings()
+        {
+            try
+            {
+                // Fetching path settings
+                Settings = SettingsDAL.GetSettings();
+
+                // Fetch current versions and update settings if needed
+                bool success = UpdateManager.CheckCurrentVersion(Settings);
+
+                if (success)
+                {
+                }
+            }
+            catch (Exception)
+            {
+                statusLabel.Foreground = Brushes.Red;
+                statusLabel.Content = "Error! Settings file not found, reinstall manager.";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                File.Copy(@"Data/OtherMods/AnyPortal.dll", String.Format("{0}BepInEx/plugins/AnyPortal.dll", Settings.ClientInstallationPath), true);
+                statusSnackBar.MessageQueue.Enqueue("AnyPortal mod installed!");
+            }
+            catch (Exception)
+            {
+                statusSnackBar.MessageQueue.Enqueue("Error! AnyPortal was not installed");
+            }
+            
         }
     }
 }
